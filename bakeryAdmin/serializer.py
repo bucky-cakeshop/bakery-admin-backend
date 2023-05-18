025b87dd21878ed5e9ef7d53ed0b7c35bd42601e
Ariel Grillo <ariel.grillo@adevinta.com>
2023-05-18 08:39:17 +0200

    Measure Unit object in ingredients response

--- bakeryAdmin/serializer.py
+++ bakeryAdmin/serializer.py
@@ -8,6 +8,11 @@ class MeasureUnitSerializer(serializers.ModelSerializer):
 
 class IngredientSerializer(serializers.ModelSerializer):
     measureUnit = serializers.PrimaryKeyRelatedField(queryset=MeasureUnit.objects.all()) 
+    measureUnit_object = serializers.SerializerMethodField()
+
+    def get_measureUnit_object(self, obj):
+        return {'id': obj.measureUnit.id, 'name': obj.measureUnit.title, 'symbol': obj.measureUnit.symbol}
+    
     class Meta:
         model = Ingredient
         fields = '__all__'
